79f57f04dd1dfb7a0d1c39d7f425b780524f3f9d
Mike Jackson <355787+minininja@users.noreply.github.com>
2021-02-23 15:38:17 -0800

    Update fib.js

--- src/js/fib.js
+++ src/js/fib.js
@@ -1,8 +1,11 @@
 function fib(x) {
-  if (x === 0 || x === 1) {
+  if (x === 0) {
+    return 0;
+  } else if (x === 1) {
     return 1;
+  } else {
+    return fib(x-1) + fib(x-2);
   }
-  return fib(x-1) + fib(x-2);
 }
 
 console.log(fib(45));
